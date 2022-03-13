ca74fdcc882f149aac97cd0cfc8456075cffa9b8
Michael Wei <mw756@cornell.edu>
2022-03-13 14:13:10 -0400

    implemented sma

--- src/indicators.ml
+++ src/indicators.ml
@@ -15,21 +15,37 @@ exception OutOfRange
 (** exception [OutOfRange] is thrown when the [sublist] method attempts
     to access an element of a list that is out of bounds *)
 
-(** [sublist start stop lst] takes in a float list [lst] along with the
-    [start] and [stop] index locations; returns the resulting sublist *)
+(** [sublist start stop lst] takes in a list [lst] along with the
+    [start] and [stop] index locations; returns the resulting sublist;
+    method altered from {{:https://stackoverflow.com/a/2717815} here} *)
 let rec sublist start stop lst =
   match lst with
   | [] -> raise OutOfRange
   | h :: t ->
       let tail =
-        if stop = 0. then [] else sublist (start -. 1.) (stop -. 1.) t
+        if stop = 0 then [] else sublist (start - 1) (stop - 1) t
       in
-      if start > 0. then tail else h :: tail
+      if start > 0 then tail else h :: tail
 
-(** [sma lst] is the simple point average; it takes in a float list and
-    returns the average as a float *)
-let sma lst =
+(** [avg lst] is the [float] average of a given [float list] *)
+let avg lst =
   let sum = List.fold_right (fun total x -> total +. x) lst 0. in
   sum /. float_of_int (List.length lst)
+
+(** [sma lst n] is the simple point average with groups of [n]
+    consecutive days; it takes in a float list and returns the averages
+    of [n] days as a float list *)
+let rec sma (lst : float list) (n : int) : float list =
+  match lst with
+  | [] -> []
+  | h :: t ->
+      if List.length lst < n then []
+      else avg (sublist 0 (n - 1) lst) :: sma t n
+(* match lst with | [] -> [] | h :: t -> let tail = if List.length lst <
+   n then [] else sma t n in if List.length lst < n then tail else avg
+   (sublist 0 n lst) :: tail *)
+(* let i = 0 in let len = List.length lst in let acc = [] in let tail =
+   avg (sublist i (i + n) lst) in if i + n >= len then acc else tail ::
+   acc *)
 
 (** [ema lst n] is the *)
--- test/michael.ml
+++ test/michael.ml
@@ -6,11 +6,16 @@ open Indicators
     [sma input]. *)
 let sma_test
     (name : string)
-    (input : float list)
-    (expected_output : float) : test =
-  name >:: fun _ ->
-  assert_equal expected_output (sma input) ~printer:string_of_float
+    (lst : float list)
+    (n : int)
+    (expected_output : float list) : test =
+  name >:: fun _ -> assert_equal expected_output (sma lst n)
 
-let sma_tests = [ sma_test "First SMA Test" [ 1.; 2.; 3.; 4.; 5. ] 3. ]
+let sma_tests =
+  [
+    sma_test "First SMA Test" [ 1.; 2.; 3.; 4.; 5. ] 2
+      [ 1.5; 2.5; 3.5; 4.5 ];
+  ]
+
 let suite = "test suite for indicators" >::: List.flatten [ sma_tests ]
 let _ = run_test_tt_main suite
