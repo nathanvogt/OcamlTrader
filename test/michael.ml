036c379e4b4d1a47d86855f5e822313e3c4e6e30
Michael Wei <mw756@cornell.edu>
2022-03-12 23:30:38 -0500

    tempt

--- test/michael.ml
+++ test/michael.ml
@@ -1,24 +1,17 @@
 open OUnit2
 open Indicators
 
-(** [index_test name input expected_output] constructs an OUnit test
+(** [sma_test name input expected_output] constructs an OUnit test
     named [name] that asserts the quality of [expected_output] with
-    [index input]. *)
+    [sma input]. *)
 let sma_test (name : string) (input : float list) (expected_output : float) :
     test =
   name >:: fun _ ->
-  (* the [printer] tells OUnit how to convert the output to a string *)
   assert_equal expected_output (sma input) ~printer:string_of_float
-
-(* You will find it helpful to write functions like [index_test] for
-   each of the other functions you are testing. They will keep your
-   lists of tests below very readable, and will also help you to avoid
-   repeating code. You will also find it helpful to create [~printer]
-   functions for the data types in use. *)
 
 let sma_tests =
   [
-    sma_test "First SMA Test" [1.; 2.; 3.; 4.; 5.] 3.;
+    sma_test "First SMA Test" [1.; 2.; 3.; 4.; 5.] 3.
   ]
 
   let suite =
