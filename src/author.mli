3c10fc211bf1811413b842a8ec62e41472015424
jren44 <juntaoren44@gmail.com>
2022-03-12 21:58:24 -0500

    new build

---
+++ src/author.mli
@@ -0,0 +1,29 @@
+(************************************************************
+   Created by Nathan Vogt, Michael Wei, and Juntao Ren
+ ************************************************************)
+
+(** CS 3110 Spring 2022 Ocaml Final Project
+
+    @author Juntao Ren (jlr429) 
+    @author Michael Wei (mw756)
+    @author Nathan Vogt (ncv7) *)
+
+(************************************************************
+
+   Academic Integrity Statement
+
+   We, the people named in the author comments above, have fully reviewed the
+   course academic integrity policies.  We have adhered to those policies in
+   creating this final project.
+
+   The policies do permit some limited collaboration among students currently
+   enrolled in the course. If we did engage in such collaboration, here is the
+   list of other students with whom we collaborated, and a brief summary of that
+   collaboration:
+
+   Signed: Juntao, Michael, and Nathan
+
+ ************************************************************)
+
+ val hours_worked : int
+ (** [hours_worked] is the number of hours you worked on this assignment. *)
