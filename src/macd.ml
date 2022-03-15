1d1eda12f2ced48eb3679ac375903f7a4970ab05
Michael Wei <mw756@cornell.edu>
2022-03-15 15:21:14 -0400

    macd implementation w/o test cases

--- src/macd.ml
+++ src/macd.ml
@@ -1,4 +1,6 @@
 let macd lst =
-  let twelve_day = Ma.ema lst 12 in
+  let ema_twelve = Ma.ema lst 12 in
+  let len = List.length ema_twelve in
+  let twelve_day = Ma.sublist 14 (len - 1) ema_twelve in
   let twenty_six_day = Ma.ema lst 26 in
   List.map2 (fun x y -> x -. y) twelve_day twenty_six_day
