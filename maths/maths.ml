9bc4dece042755c0f9782429a7eaff26be5918a1
mwei4 <mw756@cornell.edu>
2022-05-14 11:52:46 -0400

    more reformatting

--- maths/maths.ml
+++ maths/maths.ml
@@ -1,23 +1,18 @@
 let epsilon = 0.0001
 (* avoid dividing by zero *)
 
-let trend_proximity spread diff = 
-  1. /. (((diff /. spread) ** 5.) +.
- epsilon)
+let trend_proximity spread diff =
+  1. /. (((diff /. spread) ** 5.) +. epsilon)
 
 let e = 2.7182818284
+let sigmoid x = 1. /. (1. +. (1. /. (e ** x)))
 
-let sigmoid x = 
-  1. /. (1. +. (1. /. (e ** x)))
+let tanh_off o x =
+  ((e ** x) -. ((1. +. epsilon) /. ((e ** x) +. epsilon)) +. epsilon)
+  /. ((e ** x) +. ((1. +. epsilon) /. ((e ** x) +. epsilon)) +. epsilon)
+  +. o
 
-let tanh_off o x = 
-((((e ** x) -. ((1. +. epsilon) /. 
-((e ** x) +. epsilon))) +. epsilon) /. 
-(((e ** x) +. ((1. +. epsilon) /. 
-((e ** x) +. epsilon))) +. epsilon)) +. o
-
-let tanh r s x = 
-(r *. ((e ** (x /. s)) -. 
-(e ** (Float.neg (x /. s))))) /. 
-((e ** (x /. s)) +. 
-(e ** (Float.neg (x /. s))))
+let tanh r s x =
+  r
+  *. ((e ** (x /. s)) -. (e ** Float.neg (x /. s)))
+  /. ((e ** (x /. s)) +. (e ** Float.neg (x /. s)))
